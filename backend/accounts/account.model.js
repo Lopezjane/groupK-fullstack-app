0a64aa478f710cd3318cdc02854c8ddc751317b1
unknown <YY531@ccs.local>
2025-04-26 15:50:06 +0800

    modify the status

--- backend/accounts/account.model.js
+++ backend/accounts/account.model.js
@@ -17,11 +17,13 @@ function model(sequelize) {
         resetTokenExpires: { type: DataTypes.DATE },
         passwordReset: { type: DataTypes.DATE },
         created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        status: { type: DataTypes.ENUM('Active', 'Inactive'), allowNull: false, defaultValue: 'Inactive' },
         updated: { type: DataTypes.DATE },
         isVerified: {
             type: DataTypes.VIRTUAL,
             get() { return !!(this.verified || this.passwordReset); }
         }
+        
     };
 
     const options = {
